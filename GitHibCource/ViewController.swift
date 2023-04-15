6be3c5996a0e0622a100537670af87e425f9e5a6
wael al mohammad <w.almohammad@tatayab.com>
2023-04-15 13:33:20 +0300

    Initial Commit

---
+++ GitHibCource/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHibCource
+//
+//  Created by wael almohammad on 15/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("wael")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

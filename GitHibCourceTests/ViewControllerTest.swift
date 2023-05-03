585d991c3bb00223405511906894284eca46114d
wael al mohammad <w.almohammad@tatayab.com>
2023-05-03 21:56:47 +0300

    hello

--- GitHibCourceTests/ViewControllerTest.swift
+++ GitHibCourceTests/ViewControllerTest.swift
@@ -23,7 +23,7 @@ final class ViewControllerTest: XCTestCase {
     }
     
     func testValidateValue(){
-        XCTAssertEqual(true, sut?.validateValue(value: 9))
+        XCTAssertEqual(true, sut?.validateValue(value: 10))
     }
 
     func testExample() throws {

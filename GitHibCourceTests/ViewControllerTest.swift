237ef40562b6480c0b4dc1fdfe8c4421665182c6
wael al mohammad <w.almohammad@tatayab.com>
2023-05-13 11:30:59 +0300

    update build.yaml file

--- GitHibCourceTests/ViewControllerTest.swift
+++ GitHibCourceTests/ViewControllerTest.swift
@@ -22,7 +22,7 @@ final class ViewControllerTest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     func testValidateValue() {
-        XCTAssertEqual(true, sut?.validateValue(value: 9))
+        XCTAssertEqual(true, sut?.validateValue(value: 11))
     }
 
     func testExample() throws {
